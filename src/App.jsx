cd11f019e27d22358a5171cbf84b193f51be54a5
TechnicalShree <krushnasr96@gmail.com>
2022-08-07 18:56:48 +0530

    Flipkart Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Hello world..</>;
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let data = await axios.get("https://api.publicapis.org/entries");
+      let entries = data.data.entries.map((entry) => {
+        return { Description: entry.Description, isEditable: false };
+      });
+      setEntries(entries);
+      console.log(entries.length);
+    })();
+  }, []);
+
+  function editEnabled(e, entry) {
+    if (e.target.checked) {
+      entries.forEach((entryOne) => {
+        if (entryOne.Description === entry.Description) {
+          entryOne.isEditable = true;
+        }
+      });
+    } else {
+      entries.forEach((entryOne) => {
+        if (entryOne.Description === entry.Description) {
+          entryOne.isEditable = false;
+        }
+      });
+    }
+    setEntries([...entries]);
+  }
+
+  return (
+    <>
+      <h1>Discription</h1>
+      <table>
+        <tbody>
+          {entries.map((entry, idx) => (
+            <tr key={idx}>
+              <td>
+                <input
+                  type="checkbox"
+                  onChange={(e) => editEnabled(e, entry)}
+                />
+              </td>
+              <td contentEditable={entry.isEditable}>{entry.Description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
