45b336f804a13fe581c7b8cc840846176196c756
TechnicalShree <krushnasr96@gmail.com>
2022-09-25 21:30:08 +0530

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Hello world..</>;
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div>
+      <div className="todo">
+        <input type="text" placeholder="Enter the task.." />
+        <button type="button">Add todo</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
