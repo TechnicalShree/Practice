3244abd6ad155f201fba9c5eefb6aee46f89822b
TechnicalShree <krushnasr96@gmail.com>
2022-09-25 21:42:36 +0530

    this is to be reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
-
-  const clickHandler = () => {
-    setTodos(...todos, todo);
-  };
-
-  return (
-    <div>
-      <div className="todo">
-        <input
-          type="text"
-          value={todo}
-          placeholder="Enter the task.."
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="button" onClick={clickHandler}>
-          Add todo
-        </button>
-      </div>
-      <div>
-        {todos.map((todo, idx) => (
-          <p key={idx}>{todo}</p>
-        ))}
-      </div>
-    </div>
-  );
+  return <>Hello world..</>;
 }
 
 export default App;
