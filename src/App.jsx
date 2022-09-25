3a76308e3522b8070e76192ea5e7418dceddf833
TechnicalShree <krushnasr96@gmail.com>
2022-09-25 21:35:48 +0530

    added main task

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+
+  const clickHandler = () => {
+    setTodos(...todos, todo);
+  };
 
   return (
     <div>
       <div className="todo">
-        <input type="text" placeholder="Enter the task.." />
-        <button type="button">Add todo</button>
+        <input
+          type="text"
+          value={todo}
+          placeholder="Enter the task.."
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="button" onClick={clickHandler}>
+          Add todo
+        </button>
+      </div>
+      <div>
+        {todos.map((todo, idx) => (
+          <p key={idx}>{todo}</p>
+        ))}
       </div>
     </div>
   );
